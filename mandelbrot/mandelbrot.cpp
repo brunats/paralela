5b95832b29f687ee5c22dd51086a8045f1e1ce80
brunats <silvatavares.bruna@gmail.com>
2018-09-12 13:41:02 -0300

    including the run-time counter

--- mandelbrot/mandelbrot.cpp
+++ mandelbrot/mandelbrot.cpp
@@ -1,16 +1,21 @@
 #include <complex>
 #include <iostream>
-#include<omp.h>
+#include <omp.h>
+#include <sys/time.h>
+#include <math.h>
 
 using namespace std;
 
-double f_Mandelbrot(){
-    double start_time, end_time, final_time;
-    start_time = wtime();
-    int max_row, max_column, max_n;
-	cin >> max_row;
-	cin >> max_column;
-	cin >> max_n;
+double wtime()
+{
+   struct timeval t;
+   gettimeofday(&t, NULL);
+   return t.tv_sec + t.tv_usec / 1000000.0;
+}
+
+double f_Mandelbrot(int max_row, int max_column, int max_n){
+  double start_time, end_time, final_time;
+  start_time = wtime();
 
 	char **mat = (char**)malloc(sizeof(char*)*max_row);
 
@@ -40,25 +45,34 @@ double f_Mandelbrot(){
 		cout << '\n';
 	}
 	end_time = wtime();
-	final_time = end_time - start_time;
+	final_time = ((double)(end_time - start_time));
 	return final_time;
 }
 
 int main(){
-	int i, j, q_exec;
+	int i, q_exec;
 	double matT[10], total, media, desviopadrao, somatorio, mul;
-	total = 0;
 
-    q_exec = 10;
+  int max_row, max_column, max_n;
+	cin >> max_row;
+	cin >> max_column;
+	cin >> max_n;
+
+	total = 0;
+  q_exec = 10;
+  somatorio = 0;
+
 	for (i=0; i<q_exec; i++){
-        matT[i] = f_Mandelbrot();
+        matT[i] = f_Mandelbrot(max_row, max_column, max_n);
+        printf("%i - Exec\tTime:%fs\n",i+1, matT[i]);
         total += matT[i];
 	}
 	media = total/q_exec;
 	for (i=0; i<q_exec; i++){
         mul = (matT[i] - media);
-        somatorio += mul*mul;
+        somatorio += pow(mul, 2);
 	}
-
-
+  somatorio = somatorio/q_exec;
+  desviopadrao = sqrt(somatorio);
+  printf("\n\nMedia: %f\nDesvio Padrao: %f",media, desviopadrao);
 }
